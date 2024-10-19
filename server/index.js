c4c4e2ef1955c53163eb892de4ebd29edb003491
abdulrehmanwaseem <abdulrehman.code1@gmail.com>
2024-10-19 12:47:53 +0500

    Auth setup with passport and session libaries

--- server/index.js
+++ server/index.js
@@ -1,16 +1,72 @@
+import express from "express";
+import "dotenv/config.js";
+import cors from "cors";
+import http from "http";
+import compression from "compression";
+
+import passport from "passport";
+import session from "express-session";
+import connectMongoDBSession from "connect-mongodb-session";
+
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+
+import { GraphQLLocalStrategy, buildContext } from "graphql-passport";
 
 import mergedTypeDefs from "./typeDefs/index.js";
 import mergedResolvers from "./resolvers/index.js";
+import connectToDatabase from "./config/dbConnection.js";
+
+const app = express();
+
+const httpServer = http.createServer(app);
+
+const MongoDBStore = connectMongoDBSession(session);
+
+const store = new MongoDBStore({
+  uri: process.env.MONGO_URI,
+  collection: "sessions",
+});
+
+store.on("error", (err) => console.log(err));
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24, // 1 day
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+    },
+    store: store,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 const server = new ApolloServer({
   typeDefs: mergedTypeDefs,
   resolvers: mergedResolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })], // Graceful shutdown
 });
 
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 8000 }, // Set your desired port here
-});
+await server.start();
 
-console.log(`🚀 Server ready at ${url}`);
+app.use(cors());
+app.use(compression({ threshold: 1024 }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(
+  "/", // Route for the GraphQL server
+  expressMiddleware(server, {
+    context: async ({ req, res }) => buildContext({ req, res }), // Context middleware
+  })
+);
+
+await new Promise((resolve) => httpServer.listen({ port: 8000 }, resolve));
+await connectToDatabase();
+console.log(`🚀 Server ready at http://localhost:8000/`);
